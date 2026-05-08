cb80dc22170d41f416034dfe1d98a944beebac6f
Jimmy <umesshusan@gmail.com>
2026-05-08 13:30:03 +0800

    增加 Blink 的資料夾與程式

---
+++ Blink/Blink.py
@@ -0,0 +1,16 @@
+from gpiozero import LED
+from time import sleep
+
+# 定義 LED 連接在 GPIO 17 (BCM 編號)
+led = LED(17)
+
+print("LED 開始閃爍... 按下 Ctrl+C 可停止")
+
+try:
+    while True:
+        led.on()       # 點亮 LED
+        sleep(1)       # 持續 1 秒
+        led.off()      # 關閉 LED
+        sleep(1)       # 持續 1 秒
+except KeyboardInterrupt:
+    print("\n程式已停止")
