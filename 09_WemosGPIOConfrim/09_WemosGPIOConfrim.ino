821ebc06fb3901938612e726ccbd334de1f5ef54
Jimmy <umesshusan@gmail.com>
2026-07-07 23:00:37 +0800

    add GarageHomekit folder

---
+++ 09_WemosGPIOConfrim/09_WemosGPIOConfrim.ino
@@ -0,0 +1,27 @@
+void setup() {
+  Serial.begin(115200);
+  delay(500);
+  Serial.println();
+  Serial.printf("D0 = GPIO%d\n", D0);
+  Serial.printf("D1 = GPIO%d\n", D1);
+  Serial.printf("D2 = GPIO%d\n", D2);
+  Serial.printf("D3 = GPIO%d\n", D3);
+  Serial.printf("D4 = GPIO%d\n", D4);
+  Serial.printf("D5 = GPIO%d\n", D5);
+  Serial.printf("D6 = GPIO%d\n", D6);
+  Serial.printf("D7 = GPIO%d\n", D7);
+  Serial.printf("D8 = GPIO%d\n", D8);
+}
+void loop() {
+  delay(3000);
+  Serial.println();
+  Serial.printf("D0 = GPIO%d\n", D0);
+  Serial.printf("D1 = GPIO%d\n", D1);
+  Serial.printf("D2 = GPIO%d\n", D2);
+  Serial.printf("D3 = GPIO%d\n", D3);
+  Serial.printf("D4 = GPIO%d\n", D4);
+  Serial.printf("D5 = GPIO%d\n", D5);
+  Serial.printf("D6 = GPIO%d\n", D6);
+  Serial.printf("D7 = GPIO%d\n", D7);
+  Serial.printf("D8 = GPIO%d\n", D8);
+}
