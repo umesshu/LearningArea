f3f97312d19fe281220b275d30a7869fbefb34d2
Jimmy <umesshusan@gmail.com>
2026-07-10 13:12:22 +0800

    更新沒有感測器版本

--- 10_GarageHomekit/accessary.c
+++ 10_GarageHomekit/accessary.c
@@ -6,24 +6,22 @@ void my_accessory_identify(homekit_value_t _value) {
     printf("accessory identify\n");
 }
 
-// ================= 車庫門:用 Window Covering 服務(可顯示百分比) =================
-// CurrentPosition / TargetPosition: 0=全關, 100=全開
-// PositionState: 0=關閉中(遞減), 1=開啟中(遞增), 2=靜止
-homekit_characteristic_t cha_current_position =
-    HOMEKIT_CHARACTERISTIC_(CURRENT_POSITION, 0);
-homekit_characteristic_t cha_target_position =
-    HOMEKIT_CHARACTERISTIC_(TARGET_POSITION, 0);
-homekit_characteristic_t cha_position_state =
-    HOMEKIT_CHARACTERISTIC_(POSITION_STATE, 2);
+// ===== 車庫門:Garage Door Opener 服務(無感測,韌體記憶狀態)=====
+// CurrentDoorState: 0=開 1=關 2=開啟中 3=關閉中 4=停止
+// TargetDoorState : 0=開 1=關
+homekit_characteristic_t cha_current_door_state =
+    HOMEKIT_CHARACTERISTIC_(CURRENT_DOOR_STATE, 1);   // 預設關
+homekit_characteristic_t cha_target_door_state =
+    HOMEKIT_CHARACTERISTIC_(TARGET_DOOR_STATE, 1);
 homekit_characteristic_t cha_obstruction =
     HOMEKIT_CHARACTERISTIC_(OBSTRUCTION_DETECTED, false);
 
-// ================= 暫停:獨立 Switch =================
+// ===== 暫停:獨立 Switch =====
 homekit_characteristic_t cha_pause_on =
     HOMEKIT_CHARACTERISTIC_(ON, false);
 
 homekit_accessory_t *accessories[] = {
-    HOMEKIT_ACCESSORY(.id = 1, .category = homekit_accessory_category_window_covering, .services = (homekit_service_t*[]) {
+    HOMEKIT_ACCESSORY(.id = 1, .category = homekit_accessory_category_garage, .services = (homekit_service_t*[]) {
         HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics = (homekit_characteristic_t*[]) {
             HOMEKIT_CHARACTERISTIC(NAME, "車庫門"),
             HOMEKIT_CHARACTERISTIC(MANUFACTURER, "DIY"),
@@ -33,11 +31,10 @@ homekit_accessory_t *accessories[] = {
             HOMEKIT_CHARACTERISTIC(IDENTIFY, my_accessory_identify),
             NULL
         }),
-        HOMEKIT_SERVICE(WINDOW_COVERING, .primary = true, .characteristics = (homekit_characteristic_t*[]) {
+        HOMEKIT_SERVICE(GARAGE_DOOR_OPENER, .primary = true, .characteristics = (homekit_characteristic_t*[]) {
             HOMEKIT_CHARACTERISTIC(NAME, "車庫門"),
-            &cha_current_position,
-            &cha_target_position,
-            &cha_position_state,
+            &cha_current_door_state,
+            &cha_target_door_state,
             &cha_obstruction,
             NULL
         }),
